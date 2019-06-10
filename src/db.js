1970cf69deb48215aa3f5f268f5c4832dd369658
Nick <ntartaro@gmail.com>
2019-06-10 14:44:28 -0400

    backend work

---
+++ src/db.js
@@ -0,0 +1,12 @@
+// This file connects to the remote prisma DB and gives us the ability to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: "https://us1.prisma.sh/nick-tartaro-0e48c3/mph/dev",
+  // endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
